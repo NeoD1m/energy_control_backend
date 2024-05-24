42e9964494abd053e6ba9800b5d35b3bf717ce20
NeoD1m <duffman7915492@gmail.com>
2024-05-24 18:53:52 +0300

    Смена домена

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const authRoutes = require('./routes/auth');
 const fileRoutes = require('./routes/files');
